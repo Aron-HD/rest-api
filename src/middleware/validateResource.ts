8662aa43ac1a8c047461d507947d9c7853b57d36
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-19 23:55:31 +0100

    added resource validation with zod

---
+++ src/middleware/validateResource.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from 'zod';
+
+const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        schema.parse({
+            body: req.body,
+            query: req.query,
+            params: req.params
+        })
+    } catch (e: any) {
+        return res.status(400).send(e.errors)
+    }
+
+};
+
+export default validate;
