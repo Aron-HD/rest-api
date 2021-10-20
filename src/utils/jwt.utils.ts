8e70716f9df8b6179d778eb3019436f0d7ad5c25
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-20 21:56:15 +0100

    built webtoken util to sign access tokens

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+const privateKey = config.get<string>('privateKey');
+const publicKey = config.get<string>('publicKey');
+
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, privateKey, {
+    ...(options && options),
+    algorithm: 'RS256'
+  });
+}
+
+function verifyJwt(token: string) {
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    return {
+      valid: false,
+      expired: false,
+      decoded
+    };
+  } catch (e: any) {
+    return {
+      valid: false,
+      expired: e.message === 'jwt expired',
+      decoded: null
+    };
+  }
+}
