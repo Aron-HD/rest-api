e7dde58313096e06cbdd0357df8fae3711f65701
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-20 21:50:03 +0100

    built user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,23 @@
+import { object, string, TypeOf } from 'zod';
+
+export const createUserSchema = object({
+  body: object({
+    name: string({
+      required_error: 'Name is required'
+    }),
+    password: string({
+      required_error: 'Password is required'
+    }).min(6, 'Password too short - should be minimum 6 chars'),
+    passwordConfirmation: string({
+      required_error: 'passwordConfirmation is required'
+    }),
+    email: string({
+      required_error: 'Email is required'
+    }).email('Not a valid email')
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: 'Passwords do not match',
+    path: ['passwordConfirmation']
+  })
+});
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>;
