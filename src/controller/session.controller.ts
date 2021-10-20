ea0c2019d2de6c11d24a4930b77e6f27566d4cfd
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-20 23:36:58 +0100

    add find sessions function

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,29 +1,42 @@
 import { Request, Response } from 'express';
 import config from 'config';
-import { createSession } from '../service/session.service';
+import { createSession, findSessions } from '../service/session.service';
 import { validatePassword } from '../service/user.service';
 import { signJwt } from '../utils/jwt.utils';
 
 export async function createUserSessionHandler(req: Request, res: Response) {
-  // validate users password
+  // Validate the user's password
   const user = await validatePassword(req.body);
+
   if (!user) {
     return res.status(401).send('Invalid email or password');
   }
+
   // create a session
   const session = await createSession(user._id, req.get('user-agent') || '');
 
   // create an access token
+
   const accessToken = signJwt(
     { ...user, session: session._id },
-    { expiresIn: config.get('accessTokenTtl') } // 15 mins
+    { expiresIn: config.get('accessTokenTtl') } // 15 minutes
   );
 
   // create a refresh token
   const refreshToken = signJwt(
     { ...user, session: session._id },
-    { expiresIn: config.get('refreshTokenTtl') } // 15 mins
+    { expiresIn: config.get('refreshTokenTtl') } // 15 minutes
   );
+
   // return access & refresh tokens
-  res.send({ accessToken, refreshToken });
+
+  return res.send({ accessToken, refreshToken });
+}
+
+export async function getUserSessionsHandler(req: Request, res: Response) {
+  const userId = res.locals.user._id;
+
+  const sessions = await findSessions({ user: userId, valid: true });
+
+  return res.send(sessions);
 }
--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,7 +1,12 @@
-import SessionModel from '../models/session.model';
+import { FilterQuery } from 'mongoose';
+import SessionModel, { SessionDocument } from '../models/session.model';
 
 export async function createSession(userId: string, userAgent: string) {
   const session = await SessionModel.create({ user: userId, userAgent });
 
   return session.toJSON();
+}
+
+export async function findSessions(query: FilterQuery<SessionDocument>) {
+  return SessionModel.find(query).lean();
 }
