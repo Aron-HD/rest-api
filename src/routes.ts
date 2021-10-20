d2370af23eeedac108c99d464c5c81d5c56aeb2f
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-20 21:57:20 +0100

    added post routes for creating users and sessions

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,20 @@
-import { Express, Request, Response } from "express";
+import { Express, Request, Response } from 'express';
+import { createUserSessionHandler } from './controller/session.controller';
+import { createUserHandler } from './controller/user.controller';
+import validateResource from './middleware/validateResource';
+import { createSessionSchema } from './schema/session.schema';
+import { createUserSchema } from './schema/user.schema';
 
 function routes(app: Express) {
-    app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
+  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+  // request UserHandler needs to be validated with middleware
+  app.post('/api/users', validateResource(createUserSchema), createUserHandler);
+  //
+  app.post(
+    '/api/sessions',
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
 }
 
 export default routes;
