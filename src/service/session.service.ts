da3716a5773b02ede18eb6a78ac1d5c9b44f61b7
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-20 23:50:52 +0100

    add update session function

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,4 +1,4 @@
-import { FilterQuery } from 'mongoose';
+import { FilterQuery, UpdateQuery } from 'mongoose';
 import SessionModel, { SessionDocument } from '../models/session.model';
 
 export async function createSession(userId: string, userAgent: string) {
@@ -9,4 +9,11 @@ export async function createSession(userId: string, userAgent: string) {
 
 export async function findSessions(query: FilterQuery<SessionDocument>) {
   return SessionModel.find(query).lean();
+}
+
+export async function updateSession(
+  query: FilterQuery<SessionDocument>,
+  update: UpdateQuery<SessionDocument>
+) {
+  return SessionModel.updateOne(query, update);
 }
