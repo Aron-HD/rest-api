ca7554ac357deb7c0ea1a631c8005e3c58bb585b
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-21 00:32:21 +0100

    implement reissue token function in middleware

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,12 +1,19 @@
 import { get } from 'lodash';
 import { Request, Response, NextFunction } from 'express';
 import { verifyJwt } from '../utils/jwt.utils';
+import { reIssueAccessToken } from '../service/session.service';
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
+const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const accessToken = get(req, 'headers.authorization', '').replace(
     /^Bearer\s/,
     ''
   );
+
+  const refreshToken = get(req, 'headers.x-refresh');
 
   if (!accessToken) {
     return next();
@@ -16,6 +23,18 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
 
   if (decoded) {
     res.locals.user = decoded;
+    return next();
+  }
+
+  if (expired && refreshToken) {
+    const newAccessToken = await reIssueAccessToken({ refreshToken });
+
+    if (newAccessToken) {
+      res.setHeader('x-access-token', newAccessToken);
+    }
+
+    const result = verifyJwt(newAccessToken);
+    res.locals.user = result.decoded;
     return next();
   }
 
