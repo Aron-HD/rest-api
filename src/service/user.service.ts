5c34a9484e3786420daff79a5cf2747efd1d55de
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-20 21:50:42 +0100

    built user service with password validation

---
+++ src/service/user.service.ts
@@ -0,0 +1,31 @@
+import { omit } from 'lodash';
+import UserModel, { UserInput } from '../models/user.model';
+
+export async function createUser(input: UserInput) {
+  try {
+    const user = await UserModel.create(input);
+    return omit(user.toJSON(), 'password');
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function validatePassword({
+  email,
+  password
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), 'password');
+}
