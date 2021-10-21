9d7de850e3f4290349a7e5a1ca067663e3acc82e
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-21 22:06:59 +0100

    add product routes, imports and a few comments

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Express, Request, Response } from 'express';
 import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler
+} from './controller/product.controller';
+import {
   createUserSessionHandler,
   deleteSessionHandler,
   getUserSessionsHandler
@@ -7,14 +13,22 @@ import {
 import { createUserHandler } from './controller/user.controller';
 import requireUser from './middleware/requireUser';
 import validateResource from './middleware/validateResource';
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema
+} from './schema/product.schema';
 import { createSessionSchema } from './schema/session.schema';
 import { createUserSchema } from './schema/user.schema';
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
-  // request UserHandler needs to be validated with middleware
+
+  // User creation route - UserHandler is validated with middleware
   app.post('/api/users', validateResource(createUserSchema), createUserHandler);
-  //
+
+  // Session CRUD routes
   app.post(
     '/api/sessions',
     validateResource(createSessionSchema),
@@ -24,6 +38,31 @@ function routes(app: Express) {
   app.get('/api/sessions', requireUser, getUserSessionsHandler);
 
   app.delete('/api/sessions', requireUser, deleteSessionHandler);
+
+  // Product CRUD routes
+  app.post(
+    '/api/products',
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+
+  app.put(
+    '/api/products/:productId',
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.get(
+    '/api/products/:productId',
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+
+  app.delete(
+    '/api/products/:productId',
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 
 export default routes;
