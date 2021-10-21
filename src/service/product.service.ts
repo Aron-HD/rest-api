cf51cf535d369167f673da26e502de90921fcc3f
Aron <60329603+Aron-HD@users.noreply.github.com>
2021-10-21 20:38:03 +0100

    built product service to implement model

---
+++ src/service/product.service.ts
@@ -0,0 +1,28 @@
+import { FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
+import ProductModel, {
+  ProductDocument,
+  ProductInput
+} from '../models/product.model';
+
+export async function createProduct(input: ProductInput) {
+  return ProductModel.create(input);
+}
+
+export async function findProduct(
+  query: FilterQuery<ProductDocument>,
+  options: QueryOptions = { lean: true }
+) {
+  return ProductModel.findOne(query, {}, options);
+}
+
+export async function findAndUpdateProduct(
+  query: FilterQuery<ProductDocument>,
+  update: UpdateQuery<ProductDocument>,
+  options: QueryOptions
+) {
+  return ProductModel.findOneAndUpdate(query, update, options);
+}
+
+export async function deleteProduct(query: FilterQuery<ProductDocument>) {
+  return ProductModel.deleteOne(query);
+}
